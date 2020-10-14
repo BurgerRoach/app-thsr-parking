8496efbfa7007fbf796aec9a1bd09945e5ad594f
KUO, SHIN-CHI <g0936660561@gmail.com>
2020-10-14 11:46:21 +0800

    fix: test, base, city

--- test.rb
+++ test.rb
@@ -5,8 +5,11 @@ require_relative 'lib/api'
 api = THSR::Api.new
 opts = {
   'service_status': 1,
-  'service_available_level': 60,
+  'available_spaces': 60,
   'charge_status': 1
 }
+id = '2500'
 data = api.search(opts)
 print data
+park = api.search_by_park_id(id, opts)
+puts "\n\n#{park}"
