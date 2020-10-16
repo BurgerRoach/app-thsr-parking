a2523cd457b81bf0d5fba22c260cff82d69f1b4e
KUO, SHIN-CHI <g0936660561@gmail.com>
2020-10-16 16:09:14 +0800

    feat: store correct result

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,8 +2,10 @@
 
 require 'minitest/autorun'
 # require 'minitest/rg'
+require 'yaml'
 require_relative '../lib/api'
 require_relative '../lib/errors'
+CORRECT = YAML.safe_load(File.read('spec/fixtures/thsr_results.yml'))
 
 describe 'Tests THSR API library' do
   describe 'Search real time inquiry information' do
@@ -30,9 +32,11 @@ describe 'Tests THSR API library' do
   describe 'Search Park information' do
     it 'HAPPY: should provide correct search park information' do
       opts = { 'charge_status': 1 }
-      id = '2100'
-      data = THSR::Api.new.search_by_park_id(id, opts)
-      _(data['CarParkID']).must_equal id
+      testing = CORRECT.sample
+      data = THSR::Api.new.search_by_park_id(testing['CarParkID'], opts)
+      _(data['AvailableSpaces']).wont_be_nil
+      _(data['CarParkID']).must_equal testing['CarParkID']
+      _(data['CarParkName']).must_equal testing['CarParkName']
     end
 
     it 'HAPPY: should provide correct search park information when not found' do
