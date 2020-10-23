5924531631d0ab18653cf4c0ad8e4c85ec6fce4b
KUO, SHIN-CHI <g0936660561@gmail.com>
2020-10-23 23:19:22 +0800

    feat test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require 'yaml'
-
+require 'simplecov'
+SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/rg'
 require 'vcr'
@@ -10,7 +11,7 @@ require 'webmock'
 require_relative '../lib/api'
 require_relative '../lib/errors'
 
-CORRECT = YAML.safe_load(File.read('fixtures/thsr_results.yml'))
+CORRECT = YAML.safe_load(File.read('spec/fixtures/thsr_results.yml'))
 
-CASSETTES_FOLDER = 'fixtures/cassettes'
+CASSETTES_FOLDER = 'spec/fixtures/cassettes'
 CASSETTE_FILE = 'thsr_api'
