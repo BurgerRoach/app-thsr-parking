d03c8f87c46d17f18cc655c48210242a693d994d
JJwoo1125 <j6yk6vu06@gmail.com>
2020-11-01 00:00:00 +0800

    fix: refactor

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -10,6 +10,55 @@ module THSR
     plugin :assets, css: 'style.css', path: 'app/views/assets'
     plugin :halt
 
+    def to_park(route)
+      r = route
+      r.on 'park' do
+        r.is do # get params
+          r.post do
+            park_id = r.params['park_id']
+            r.redirect "/result/park/#{park_id}"
+          end
+        end
+        pass_park(route)
+      end
+    end
+
+    def pass_park(route)
+      r = route
+      r.on String do |park_id|
+        # GET /park/park_id
+        r.get do
+          api = THSRParking::THSR::Api.new
+          data = api.search_by_park_id(park_id)
+          time = data['update_time']
+          parks = data['parks']
+          view 'result', locals: { result: parks, time: time }
+        end
+      end
+    end
+
+    def to_city(route)
+      r = route
+      r.on 'city' do
+        r.is do # get params
+          r.post do
+            city_name = r.params['city_name']
+
+            pass_city(city_name)
+          end
+        end
+      end
+    end
+
+    def pass_city(city_name)
+      api = THSRParking::THSR::Api.new
+      data = api.search_by_city(city_name)
+      time = data['update_time']
+      parks = data['parks']
+      view 'result', locals: { result: parks, time: time }
+      # r.redirect "/result/city?city_name=#{city_name}"
+    end
+
     route do |r|
       r.assets # load CSS
 
@@ -20,59 +69,10 @@ module THSR
 
       r.on 'result' do
         # GET /result/park/park_id
-        r.on 'park' do
-          r.is do # get params
-            r.post do
-              park_id = r.params['park_id']
-              r.redirect "/result/park/#{park_id}"
-            end
-          end
-
-          r.on String do |park_id|
-            # GET /park/park_id
-            r.get do
-              api = THSRParking::THSR::Api.new
-              data = api.search_by_park_id(park_id)
-              parks = data['parks']
-
-              view 'result', locals: { result: parks }
-            end
-          end
-        end
+        to_park(r)
 
         # GET /result/city/city_name
-        r.on 'city' do
-          r.is do # get params
-            r.post do
-              city_name = r.params['city_name']
-              puts city_name
-
-              api = THSRParking::THSR::Api.new
-              data = api.search_by_city(city_name)
-              parks = data['parks']
-
-              puts parks
-
-              view 'result', locals: { result: parks }
-
-              # r.redirect "/result/city/#{city_name}"
-            end
-          end
-
-          # r.on String do |city_name|
-          #   puts city_name
-          #   # GET /result/city_name
-          #   r.get do
-          #     api = THSRParking::THSR::Api.new
-          #     data = api.search_by_city(city_name)
-          #     parks = data['parks']
-
-          #     puts parks
-
-          #     view 'result', locals: { result: parks }
-          #   end
-          # end
-        end
+        to_city(r)
       end
     end
   end
