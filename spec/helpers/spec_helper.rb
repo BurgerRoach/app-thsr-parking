47abf1168894e40b2aa737e5b272d9cc04d1930d
JJwoo1125 <j6yk6vu06@gmail.com>
2020-12-18 09:17:00 +0800

    fix tests_acceptance

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-ENV['RACK_ENV'] = 'test'
 
+ENV['RACK_ENV'] = 'test'
 
 require 'yaml'
 require 'simplecov'
@@ -14,18 +14,17 @@ require 'minitest/rg'
 require_relative '../../lib/api'
 require_relative '../../lib/errors'
 
-
 require_relative '../../config/environment'
 require_relative '../../app/init'
-require_relative '../../app/infrastructure/gateways/api'
-require_relative '../../app/domain/restaurant/repositories/locations'
+# require_relative '../../app/infrastructure/gateways/api'
+# require_relative '../../app/domain/restaurant/repositories/locations'
 
 # CORRECT = YAML.safe_load(File.read('spec/fixtures/thsr_results.yml'))
 
-# CASSETTES_FOLDER = 'spec/fixtures/cassettes'
-# CASSETTE_FILE = 'thsr_api'
+CASSETTES_FOLDER = 'spec/fixtures/cassettes'
+CASSETTE_FILE = 'thsr_api'
 
 # Helper methods
 def homepage
-    THSRParking::App.config.APP_HOST
+  THSRParking::App.config.APP_HOST
 end
