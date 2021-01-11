764978ea7a0244bf86b0b534793428e4afd4938f
music1353 <music1353@gmail.com>
2021-01-11 10:25:23 +0800

    refactor: delete useless code

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -26,26 +26,12 @@ module THSRParking
     def pass_city(route, city_name)
       parks_made = Service::Parks.new.call(city_name)
 
-      # timetable_made = Service::Timetable.new.call({
-      #   'city_name': city_name,
-      #   'date': Date.today,
-      #   'direction': '0'
-      # })
-
       if parks_made.failure?
         flash[:error] = parks_made.failure
         route.redirect '/'
       end
 
-      # if timetable_made.failure?
-      #   flash[:error] = timetable_made.failure
-      #   route.redirect '/'
-      # end
-
       parks = parks_made.value!
-      # timetables = timetable_made.value!
-
-      # puts timetables
 
       flash.now[:notice] = 'No match result' if parks.length.zero?
 
